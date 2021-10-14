04a3a1e56373cc91967c09c06d1f9a2e8682556a
Defu <gudo35@gmail.com>
2021-10-14 19:16:11 +0200

    Implementation uses a vector to avoid collisions. Need to remove used entries

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,19 @@ extern crate clap;
 use clap::App;
 
 use std::net::{SocketAddr, ToSocketAddrs, UdpSocket};
-use std::collections::{BTreeMap};
+use std::ops::Index;
 
 struct Packet{
     buffer: Vec<u8>,
-    dest: SocketAddr
+    dest: SocketAddr,
+    sent_date : u128,
+    sent : bool
 }
 impl Packet
 {
-    fn new(buffer : Vec<u8>, dest : SocketAddr) -> Packet{
+    fn new(buffer : Vec<u8>, dest : SocketAddr, sent_date : u128) -> Packet{
         Packet{
-            buffer, dest
+            buffer, dest, sent_date, sent : false
         }
     }
 
@@ -43,7 +45,7 @@ fn main() {
     println!("The listen port is {}", listen_port);
     println!("Datagrams will be redirected to port {}", connect_port);
 
-    let mut packet_queue : BTreeMap<u128, Packet> = BTreeMap::new();
+    let mut packet_queue : Vec<Packet> = Vec::new();
 
     let server_address = ToSocketAddrs::to_socket_addrs(&format!("127.0.0.1:{}", connect_port)[..]).expect("Invalid connection port").next().unwrap();
     let socket = UdpSocket::bind(format!("0.0.0.0:{}", listen_port)).expect(&format!("Could not bind to UDP port {}", listen_port)[..]);
@@ -67,24 +69,29 @@ fn main() {
             let from_server = src.port() == connect_port;
             let dest = if from_server {client_src} else {server_address};
             println!("Packet recv from {} of size {} to be sent to {}", src, size, dest);
-            let packet = Packet::new(buffer, dest);
 
             let now = now_millis();
-            const NETWORK_DELAY: u128 = 10;
+            const NETWORK_DELAY: u128 = 50;
             let sent_date = now + NETWORK_DELAY;
-            packet_queue.insert(sent_date, packet);
+            println!("Date to send {}", sent_date);
+
+            let packet = Packet::new(buffer, dest, sent_date);
+
+            packet_queue.push(packet);
         }
         
         let now = now_millis();
-        let to_keep = packet_queue.split_off(&now);
-
-        for (date, packet) in &packet_queue {
-            assert!(*date < now, "Date wasnt ready to be sent");
-            
-            let delay = now - date;
-            println!("Packet sent to {} of size {} with delay: {} ms", packet.dest, packet.size(), delay);
-            socket.send_to(&packet.buffer, packet.dest).expect("Error while sending");
+        for packet in packet_queue.iter_mut()
+        {
+            let date = packet.sent_date;
+            if date <  now && !packet.sent
+            {
+                let delay = now - date;
+                println!("Packet sent to {} of size {} with delay: {} ms", packet.dest, packet.size(), delay);
+                socket.send_to(&packet.buffer, packet.dest).expect("Error while sending");
+                packet.sent = true;
+            }
         }
-        packet_queue = to_keep;
+        
     }
 }
