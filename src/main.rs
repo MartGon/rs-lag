35d5f1f17b01ab9c3c205fb3c597e0fed29451de
Defu <gudo35@gmail.com>
2021-10-16 16:52:41 +0200

    Implements duplicated packets

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@
 #[macro_use]
 extern crate clap;
 use clap::App;
+use rand::prelude::{Distribution, ThreadRng};
 
 use std::mem::swap;
 use std::net::{ToSocketAddrs, UdpSocket};
 use std::collections::BTreeMap;
+
+use rand::distributions::{Uniform};
+use std::convert::TryFrom;
 
 mod network;
 use crate::network::Packet;
@@ -22,6 +26,20 @@ fn millis_to_nanos(millis : u128) -> u128{
     millis * 1_000_000
 }
 
+fn i128_millis_to_nanos(millis : i128) -> i128
+{
+    millis * 1_000_000
+}
+
+fn gen_delay(lag: u128, dist : Uniform::<i128>, rng : &mut ThreadRng) -> u128
+{
+    let now = now_nanos();
+    let network_delay: u128 = millis_to_nanos(lag);
+    let jitter = i128_millis_to_nanos(dist.sample(rng));
+    let sent_date = u128::try_from(now as i128  + network_delay as i128 + jitter).expect("Conversion error");
+    return sent_date;
+}
+
 fn main() {
 
     let yaml = load_yaml!("cli.yml");
@@ -34,7 +52,13 @@ fn main() {
     let connect_port = connect_port.parse::<u16>().unwrap();
 
     let lag = matches.value_of("lag").unwrap();
-    let lag = lag.parse::<u128>().unwrap();
+    let lag = lag.parse::<u128>().unwrap() / 2;
+
+    let jitter = matches.value_of("jitter").unwrap();
+    let jitter : i128 = jitter.parse::<i128>().unwrap() / 2;
+
+    let duplication_chance = matches.value_of("duplication").unwrap();
+    let duplication_chance : f32 = std::cmp::min(std::cmp::max(duplication_chance.parse::<i32>().unwrap(), 0), 100) as f32;
     
     println!("The listen port is {}", listen_port);
     println!("Datagrams will be redirected to port {}", connect_port);
@@ -43,6 +67,10 @@ fn main() {
 
     let server_address = ToSocketAddrs::to_socket_addrs(&format!("127.0.0.1:{}", connect_port)[..]).expect("Invalid connection port").next().unwrap();
     let socket = UdpSocket::bind(format!("0.0.0.0:{}", listen_port)).expect(&format!("Could not bind to UDP port {}", listen_port)[..]);
+
+    let jitter_dist = Uniform::<i128>::from(-jitter..(jitter + 1));
+    let duplication_dist = Uniform::<f32>::from(0f32..100f32);
+    let mut rng = rand::thread_rng();
 
     // Get client address from first packet. Wait for it
     let mut buf = [0; 255];
@@ -62,15 +90,18 @@ fn main() {
 
             let from_server = src.port() == connect_port;
             let dest = if from_server {client_src} else {server_address};
+            let sent_date = gen_delay(lag, jitter_dist, &mut rng);
             println!("Packet recv from {} of size {} to be sent to {}", src, size, dest);
 
-            let now = now_nanos();
-            let network_delay: u128 = millis_to_nanos(lag);
-            let sent_date = now + network_delay;
-            println!("Date to send {}", sent_date);
+            let roll = duplication_dist.sample(&mut rng);
+            if roll < duplication_chance
+            {
+                let sent_date = gen_delay(lag, jitter_dist, &mut rng);
+                let duplicate = Packet::new(buffer.clone(), dest, sent_date);
+                packet_queue.insert(sent_date, duplicate);
+            }
 
             let packet = Packet::new(buffer, dest, sent_date);
-
             packet_queue.insert(sent_date, packet);
         }
         
--- src/network.rs
+++ src/network.rs
@@ -1,5 +1,5 @@
 
-use std::net::SocketAddr;
+use std::{net::SocketAddr};
 
 pub struct Packet{
     pub buffer: Vec<u8>,
