d7d19583e951f5ee635152d8ab06c0f14b7d1bda
Defu <gudo35@gmail.com>
2021-10-29 17:35:30 +0200

    Allows configuration of the two channels
    Threads are dropped when disconnection occurs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use clap::App;
 
 
 use std::net::{ToSocketAddrs};
+use std::time::Duration;
 
 mod network;
 mod proxy;
@@ -18,14 +19,14 @@ fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    let listen_port = matches.value_of("listen-port").unwrap();
-    let listen_port = listen_port.parse::<u16>().unwrap();
+    let bind_port = matches.value_of("bind-port").unwrap();
+    let bind_port = bind_port.parse::<u16>().unwrap();
 
-    let connect_port = matches.value_of("connect-port").unwrap();
-    let connect_port = connect_port.parse::<u16>().unwrap();
+    let connect_addr = matches.value_of("connect-addr").unwrap();
+    let server_addr = ToSocketAddrs::to_socket_addrs(&connect_addr).expect("Could not convert connect-addr").next().unwrap();
 
-    println!("The listen port is {}", listen_port);
-    println!("Datagrams will be redirected to port {}", connect_port);
+    println!("The listen port is {}", bind_port);
+    println!("Datagrams will be redirected to {}", connect_addr);
 
     let lag = matches.value_of("lag").unwrap();
     let lag = std::cmp::max((lag.parse::<u128>().unwrap()) / 2, 0);
@@ -35,30 +36,42 @@ fn main() {
     let jitter : i128 = jitter.parse::<i128>().unwrap();
     println!("The RTT will have {} ms variance", jitter);
 
-    let duplication_chance = matches.value_of("duplication").unwrap();
-    let duplication_chance : f32 = duplication_chance.parse::<f32>().unwrap().min(100f32).max(0f32);
-    println!("Packet Duplication chance: {}", duplication_chance);
+    let cs_duplication_chance = matches.value_of("client-server-duplication").unwrap();
+    let cs_duplication_chance : f32 = cs_duplication_chance.parse::<f32>().unwrap().min(100f32).max(0f32);
+    println!("Client->Server Packet Duplication chance: {}", cs_duplication_chance);
 
-    let unorder_chance = matches.value_of("unorder").unwrap();
-    let unorder_chance : f32 = unorder_chance.parse::<f32>().unwrap().min(100f32).max(0f32);
-    println!("Packet Unorder chance: {}", unorder_chance);
+    let cs_unorder_chance = matches.value_of("client-server-unorder").unwrap();
+    let cs_unorder_chance : f32 = cs_unorder_chance.parse::<f32>().unwrap().min(100f32).max(0f32);
+    println!("Client->Server Packet Unorder chance: {}", cs_unorder_chance);
 
-    let loss_chance = matches.value_of("loss").unwrap();
-    let loss_chance : f32 = loss_chance.parse::<f32>().unwrap().min(100f32).max(0f32);
-    println!("Packet Loss chance: {}", loss_chance);
+    let cs_loss_chance = matches.value_of("client-server-loss").unwrap();
+    let cs_loss_chance : f32 = cs_loss_chance.parse::<f32>().unwrap().min(100f32).max(0f32);
+    println!("Client->Server Packet Loss chance: {}", cs_loss_chance);
+
+    let sc_duplication_chance = matches.value_of("server-client-duplication").unwrap();
+    let sc_duplication_chance : f32 = sc_duplication_chance.parse::<f32>().unwrap().min(100f32).max(0f32);
+    println!("Server->Client Packet Duplication chance: {}", sc_duplication_chance);
+
+    let sc_unorder_chance = matches.value_of("server-client-unorder").unwrap();
+    let sc_unorder_chance : f32 = sc_unorder_chance.parse::<f32>().unwrap().min(100f32).max(0f32);
+    println!("Server->Client Packet Unorder chance: {}", sc_unorder_chance);
+
+    let sc_loss_chance = matches.value_of("server-client-loss").unwrap();
+    let sc_loss_chance : f32 = sc_loss_chance.parse::<f32>().unwrap().min(100f32).max(0f32);
+    println!("Server->Client Packet Loss chance: {}", sc_loss_chance);
+
+    let timeout = matches.value_of("timeout").unwrap();
+    let timeout = timeout.parse::<u64>().unwrap();
+    println!("Timeout : {}", timeout);
 
     // Initializing proxy
-    let conditions = Conditions::new(lag, jitter, duplication_chance, unorder_chance, loss_chance);
+    let server_client_conditions = Conditions::new(lag, jitter, sc_duplication_chance, sc_unorder_chance, sc_loss_chance);
+    let client_server_conditions = Conditions::new(lag, jitter, cs_duplication_chance, cs_unorder_chance, cs_loss_chance);
 
-    let proxy_addr = format!("0.0.0.0:{}", listen_port);
+    let proxy_addr = format!("0.0.0.0:{}", bind_port);
     let proxy_addr = ToSocketAddrs::to_socket_addrs(&proxy_addr).expect("Could not convert addr").next().unwrap();
-    let server_addr = format!("127.0.0.1:{}", connect_port);
-    let server_addr = ToSocketAddrs::to_socket_addrs(&server_addr).expect("Could not convert addr").next().unwrap();
-    
-    let mut client_proxy_cond = conditions.clone();
-    client_proxy_cond.loss_chance = 0f32;
-    
-    let server_proxy_cond = conditions;
-    let mut proxy = Proxy::new(proxy_addr, server_addr, client_proxy_cond, server_proxy_cond);
+
+    let mut proxy = Proxy::new(proxy_addr, server_addr, 
+        client_server_conditions, server_client_conditions, Duration::from_millis(timeout));
     proxy.run();
 }
--- src/proxy.rs
+++ src/proxy.rs
@@ -1,4 +1,4 @@
-use std::{collections::{BTreeMap, HashMap}, mem::swap, net::{SocketAddr, UdpSocket}, sync::{Arc, Condvar, Mutex}};
+use std::{collections::{BTreeMap, HashMap}, mem::swap, net::{SocketAddr, UdpSocket}, sync::{Arc, Condvar, Mutex}, time::Duration};
 
 use crate::{network::Packet};
 use crate::conditions::Conditions;
@@ -8,29 +8,28 @@ pub struct Proxy{
         SocketAddr, 
         Arc::<(Mutex::<BTreeMap::<u128, Packet>>, Condvar)>
     >,
-    client_sockets : HashMap<SocketAddr, Arc::<UdpSocket>>,
 
     server_socket : Arc::<UdpSocket>,
     address : SocketAddr,
     server_address : SocketAddr,
 
     client_proxy_conditions : Conditions,
-    server_proxy_conditions : Conditions
+    server_proxy_conditions : Conditions,
+    timeout : Duration,
 }
 
 impl Proxy{
 
-    pub fn new(address : SocketAddr, server_address : SocketAddr, client_proxy_conditions : Conditions, server_proxy_conditions : Conditions) -> Proxy{
+    pub fn new(address : SocketAddr, server_address : SocketAddr, client_proxy_conditions : Conditions, server_proxy_conditions : Conditions, timeout : Duration) -> Proxy{
         let packet_queues = HashMap::<SocketAddr, Arc::<(Mutex::<BTreeMap::<u128, Packet>>, Condvar)>>::new();
-        let client_sockets = HashMap::<SocketAddr, Arc::<UdpSocket>>::new();
         Proxy{
             packet_queues,
-            client_sockets,
             server_socket: Arc::new(UdpSocket::bind(address).expect(&format!("Could not bind to UDP {}", address))),
             address,
             server_address,
             client_proxy_conditions,
             server_proxy_conditions,
+            timeout
         }
     }
 
@@ -58,7 +57,7 @@ impl Proxy{
             if let Ok((size, src)) = res
             {
                 buffer.truncate(size);
-                if !self.client_sockets.contains_key(&src)
+                if !self.packet_queues.contains_key(&src)
                 {
                     self.add_client(src);
                     println!("Client connected from {}", src);
@@ -78,13 +77,13 @@ impl Proxy{
     fn add_client(&mut self, client_address : SocketAddr)
     {
         // Create new socket address
-        let port = self.address.port() + self.client_sockets.len() as u16 + 1;
+        let port = self.address.port() + self.packet_queues.len() as u16 + 1;
         let mut client_socket_addr = self.address;
         client_socket_addr.set_port(port);
 
         // Crate client socket
         let client_socket = Arc::new(UdpSocket::bind(&client_socket_addr).expect(&format!("Could not bind to dynamic address {}", client_socket_addr)));
-        self.client_sockets.insert(client_address, client_socket.clone());
+        client_socket.set_read_timeout(Some(self.timeout)).expect("Error on set read timeout");
 
         // Create packet queue
         let queue = (Mutex::new(BTreeMap::<u128, Packet>::new()), Condvar::new());
@@ -96,30 +95,37 @@ impl Proxy{
         let queue_recv = self.get_server_queue().clone();
         let client_socket_recv = client_socket.clone();
         let _handle = std::thread::spawn(move || {
-            loop{
-                Proxy::recv_inc_packets(&queue_recv, &client_socket_recv, client_address, conditions);
+            while Proxy::recv_inc_packets(&queue_recv, &client_socket_recv, client_address, conditions)
+            {
             }
+            println!("Client disconnected from {}", client_address);
         });
 
         // Proxy -> Server Listening thread
         let queue_send = queue.clone();
         let client_socket_send = client_socket.clone();
         let _handle = std::thread::spawn(move ||{
-            loop{
+            while Arc::strong_count(&client_socket_send) > 1
+            {
                 Proxy::send_pending_packets(&queue_send, &client_socket_send);
             }
         });
     }
 
-    fn recv_inc_packets(queue : &Arc::<(Mutex::<BTreeMap::<u128, Packet>>, Condvar)>, socket : &Arc<UdpSocket>, dest : SocketAddr, conditions : Conditions)
+    fn recv_inc_packets(queue : &Arc::<(Mutex::<BTreeMap::<u128, Packet>>, Condvar)>, socket : &Arc<UdpSocket>, dest : SocketAddr, conditions : Conditions) -> bool
     {
         let mut buffer : Vec<u8> = vec![0; 255];
         let res = socket.recv_from(&mut buffer);
-        let (size, _src) = res.expect("Recv from socket failed"); 
-        buffer.truncate(size);
-        
-        //println!("Recv packet from server {} with destiny {}", src, dest);
-        Proxy::queue_packet_static(&queue, buffer, dest, conditions);
+        let handled = res.is_ok();
+        if let Some((size, _src)) = res.ok()
+        {
+            buffer.truncate(size);
+
+            //println!("Recv packet from server {} with destiny {}", _src, dest);
+            Proxy::queue_packet_static(&queue, buffer, dest, conditions);
+        }
+
+        return handled;
     }
 
     fn send_pending_packets(queue : &Arc::<(Mutex::<BTreeMap::<u128, Packet>>, Condvar)>, socket : &Arc<UdpSocket>)
@@ -181,7 +187,14 @@ impl Proxy{
         let packet = Packet::new(buffer, dest, sent_date);
         let (q, cvar) = &**packet_queue;
         let mut q = q.lock().unwrap();
-        q.insert(sent_date, packet);
-        cvar.notify_one();
+        if !q.contains_key(&sent_date)
+        {
+            q.insert(sent_date, packet);
+            cvar.notify_one();
+        }
+        else
+        {
+            println!("Error: Queue already contains a packet for that date");
+        }
     }
 }
